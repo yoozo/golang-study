09519d5959ef9e682d9acf2d7d8bc6909782803b
tanyongzhao <tanyongzhao@tuzhanai.com>
2024-02-29 10:41:33 +0800

    依赖包 镜像源

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ func main() {
 	fmt.Println("Hello, World!", http.StatusOK, quote.Go())
 }
 
+// 七牛 CDN
+// go env -w  GOPROXY=https://goproxy.cn,direct
+
 // go mod init <package>	初始化程序包 go.mod
 
 // go mod tidy    检查缺失的依赖并安装 go.sum
